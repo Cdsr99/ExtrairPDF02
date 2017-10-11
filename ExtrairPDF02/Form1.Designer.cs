6ec31e9a0734d96304649e711cc4852d6d4da92b
Cdsr99 <31573978+Cdsr99@users.noreply.github.com>
2017-10-11 09:41:41 -0300

    11/10/2017

--- ExtrairPDF02/Form1.Designer.cs
+++ ExtrairPDF02/Form1.Designer.cs
@@ -28,12 +28,335 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarLinkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblines = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbpdf = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbpng = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbmp3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.html = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(727, 359);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(727, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salvarLinkToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // salvarLinkToolStripMenuItem
+            // 
+            this.salvarLinkToolStripMenuItem.Name = "salvarLinkToolStripMenuItem";
+            this.salvarLinkToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.salvarLinkToolStripMenuItem.Text = "Salvar Link";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(300, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Choose the HTML File";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(0, 54);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "HTTP";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(61, 54);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "HTTPS";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(129, 54);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(47, 17);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "PDF";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(182, 54);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(49, 17);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "PNG";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(237, 54);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(48, 17);
+            this.checkBox5.TabIndex = 9;
+            this.checkBox5.Text = "MP3";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 443);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Lines:";
+            // 
+            // lblines
+            // 
+            this.lblines.AutoSize = true;
+            this.lblines.Location = new System.Drawing.Point(53, 443);
+            this.lblines.Name = "lblines";
+            this.lblines.Size = new System.Drawing.Size(19, 13);
+            this.lblines.TabIndex = 11;
+            this.lblines.Text = "....";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 443);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "PDF:";
+            // 
+            // lbpdf
+            // 
+            this.lbpdf.AutoSize = true;
+            this.lbpdf.Location = new System.Drawing.Point(125, 443);
+            this.lbpdf.Name = "lbpdf";
+            this.lbpdf.Size = new System.Drawing.Size(19, 13);
+            this.lbpdf.TabIndex = 13;
+            this.lbpdf.Text = "....";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 443);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "PNG:";
+            // 
+            // lbpng
+            // 
+            this.lbpng.AutoSize = true;
+            this.lbpng.Location = new System.Drawing.Point(198, 443);
+            this.lbpng.Name = "lbpng";
+            this.lbpng.Size = new System.Drawing.Size(19, 13);
+            this.lbpng.TabIndex = 15;
+            this.lbpng.Text = "....";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(234, 443);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "MP3";
+            // 
+            // lbmp3
+            // 
+            this.lbmp3.AutoSize = true;
+            this.lbmp3.Location = new System.Drawing.Point(273, 443);
+            this.lbmp3.Name = "lbmp3";
+            this.lbmp3.Size = new System.Drawing.Size(19, 13);
+            this.lbmp3.TabIndex = 17;
+            this.lbmp3.Text = "....";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Clear all";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(680, 443);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "v1.0.1";
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // html
+            // 
+            this.html.AutoSize = true;
+            this.html.Location = new System.Drawing.Point(291, 54);
+            this.html.Name = "html";
+            this.html.Size = new System.Drawing.Size(45, 17);
+            this.html.TabIndex = 20;
+            this.html.Text = "html";
+            this.html.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(642, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Programmer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(727, 465);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.html);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbmp3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbpng);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbpdf);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblines);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Extrair Mídias";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarLinkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblines;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbpdf;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbpng;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbmp3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.CheckBox html;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- ExtrairPDF02/Form1.cs
+++ ExtrairPDF02/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,163 @@ namespace ExtrairPDF02
 {
     public partial class Form1 : Form
     {
+        String user = System.Environment.GetEnvironmentVariable("USERNAME");
         public Form1()
         {
             InitializeComponent();
+            atributos();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void test()
+        {
+            if (checkBox1.Checked && html.Checked)
+            {
+                MessageBox.Show("Boa garoto!");
+            }
+        }
+        public void Abrir_html()
+        {
+            ofd.InitialDirectory = comboBox1.Text;
+            ofd.FileName = "";
+            ofd.Filter = "HTML (*.html)|*.html";
+            //ofd.Filter = "Texto (*.txt)|*.txt";
+            ofd.Title = "Select HTML File";
+            ofd.ShowDialog();
+
+            try
+            {
+                ler();
+            }
+            catch (Exception ex)
+            {
+
+               MessageBox.Show("Erro" + ex.ToString());
+            }
+
+
+        }
+        #region atributos
+        public void atributos()
+        {
+
+            
+            comboBox1.Items.Add("C:\\");
+            comboBox1.Items.Add("C:\\Users\\" + user + "\\desktop");
+            comboBox1.Items.Add("C:\\Users\\"+ user + "\\Documents");
+            comboBox1.Items.Add("C:\\Users\\"+ user +"\\Downloads");          
+           
+        }
+        #endregion
+        #region ler
+        public void ler()
+        {
+            String caminho = ofd.FileName;
+            String content;
+            String extensão = ".pdf";
+            String http = "http://";
+            String href = "href=";
+            String line;
+            // String content2;
+
+            int contline =0 ;
+            try
+            {
+                StreamReader arq = new StreamReader(caminho);
+                line = content;
+
+                while ((content = arq.ReadLine()) != null)
+                {
+                   richTextBox1.AppendText(content);
+                    richTextBox1.AppendText("\n");
+                    contline++;
+                    lblines.Text = Convert.ToString(contline);
+                    if (richTextBox1.Text.Contains(href) && richTextBox1.Text.Contains(http) && richTextBox1.Text.Contains(extensão))
+                    {
+                        MessageBox.Show("Achei um link #eggs");
+                    }
+                }
+               
+               
+                arq.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+               // MessageBox.Show("Erro " + ex.ToString());
+            }
+            
+            
+        }
+        #endregion
+        #region Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Abrir_html();
+            
+        }
+        #endregion
+        #region limpar
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            lblines.Text = "....";
+            lbmp3.Text = "....";
+            lbpdf.Text = "....";
+            lbpng.Text = "....";
+            comboBox1.Text = "";
+        }
+        #endregion
+        #region caça_palavra
+        public void caça_palavra()
+        {
+            
+            try
+            {
+                String html = "html";
+                String caminho = ofd.FileName;
+                StreamReader arq = new StreamReader(caminho);
+                //StreamReader ler_combobox = new StreamReader(comboBox1.Text);
+                //ler_combobox.ReadToEnd();
+                //Convert.ToString(content);
+                String content;
+                if (richTextBox1.Text.Contains("html"))
+                {
+                    richTextBox1.Find(html);
+                    richTextBox1.SelectionColor = Color.Red;
+                }
+                arq.Close();
+                
+               // ler_combobox.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro:404 html not found! "/* + ex.ToString()*/);
+                
+            }
+        }
+
+        #endregion
+        #region link
+        public void link()
+        {
+            String caminho = ofd.FileName;
+
+            StreamReader str = new StreamReader(caminho);
+
+        }
+        #endregion
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            caça_palavra();
         }
     }
 }
